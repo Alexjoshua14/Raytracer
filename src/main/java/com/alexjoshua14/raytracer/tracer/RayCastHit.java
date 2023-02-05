a664df72885b472795369bd8b85d854f9345ae62
Alex <ajoshua14@icloud.com>
2023-02-05 15:43:15 -0800

    Add class for ray cast hits

---
+++ src/main/java/com/alexjoshua14/raytracer/tracer/RayCastHit.java
@@ -0,0 +1,44 @@
+package com.alexjoshua14.raytracer.tracer;
+
+import com.alexjoshua14.raytracer.scene.*;
+
+public class RayCastHit {
+    float t;
+    SceneObject obj;
+    Vector3 normal;
+
+    public RayCastHit(float t, SceneObject obj) {
+        this.t = t;
+        this.obj = obj;
+    }
+
+    public RayCastHit(float t, SceneObject obj, Vector3 normal) {
+        this.t = t;
+        this.obj = obj;
+        this.normal = normal;
+    }
+
+    public float getT() {
+        return t;
+    }
+
+    public SceneObject getObject() {
+        return this.obj;
+    }
+
+    public Vector3 getNormal() {
+        return this.normal;
+    }
+
+    public void setT(float t) {
+        this.t = t;
+    }
+
+    public void setObject(SceneObject obj) {
+        this.obj = obj;
+    }
+
+    public void setNormal(Vector3 normal) {
+        this.normal = normal;
+    }
+}
